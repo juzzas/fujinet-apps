c8c1b95d8501d723b3efc032a272b356d2694498
Justin Skists <jskists@roku.com>
2023-08-20 16:35:34 +0100

    rc2014: netcat: refactor for new libraries

--- netcat/rc2014/src/main.c
+++ netcat/rc2014/src/main.c
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include "console.h"
 #include "fujinet.h"
+#include "fujinet_logical.h"
 #include "fujinet_network.h"
 
 unsigned char buf[1024];
@@ -16,13 +18,18 @@ unsigned char t=0;
 struct network_status status;
 static bool done = false;
 
+uint8_t network_unit = 0;
 char url[256];
+
+FUJINET_LOGICAL_DEVICE_TYPE src_logical_type = FUJINET_LOGICAL_DEVICE_TYPE_UNKNOWN;
+uint8_t src_logical_unit = 0;
+const char* src_logical_url = NULL;
 
 
 void in(void)
 {
     if (fujinet_poll_proceed()) {
-        FUJINET_RC rc = fujinet_network_status(url, &status);
+        FUJINET_RC rc = fujinet_network_status(src_logical_unit, &status);
 
         if (rc == FUJINET_RC_OK) {
             uint16_t l = (status.bytes_waiting_high << 8) + status.bytes_waiting_low;
@@ -31,7 +38,7 @@ void in(void)
 
             //printf("reading %u bytes\n", l);
             if (l > 0) {
-                rc = fujinet_network_read(url, buf, l);
+                rc = fujinet_network_read(src_logical_unit, buf, l);
                 //printf("read %u bytes (%u)\n", l, rc);
                 if (rc == FUJINET_RC_OK) {
                     for (uint16_t i = 0; i < l; i++) {
@@ -53,7 +60,7 @@ void out(void)
 	if (txbuf[0] == 26) {
 		done = true;
 	} else {
-		fujinet_network_write(url, txbuf, 1); // send one char.
+		fujinet_network_write(src_logical_unit, txbuf, 1); // send one char.
 	}
     }
 }
@@ -63,23 +70,34 @@ int main(int argc, char **argv)
 //    clrscr();
     fujinet_init();
 
-    printf("Welcome to netcat, an example #FujiNet Network Program\nThis is a very simple 80 column terminal program.\n\nAvailable protocols: TCP UDP TELNET HTTP/S TNFS FTP\n\nURL's are of the form:\n\nN:PROTO://host.name:port/path?key=val&key=val\n\nExamples of URL's:\n------------------\nN:TELNET://BBS.FOZZTEXX.NET:23/\nN:HTTPS://www.gnu.org/licenses/gpl-3.0.txt\n\n"); 
+    printf("Welcome to netcat, an example #FujiNet Network Program\n"
+           "This is a very simple 80 column terminal program.\n\n"
+           "Available protocols: TCP UDP TELNET HTTP/S TNFS FTP\n\n"
+           "URL's are of the form:\n\n"
+           "PROTO://host.name:port/path?key=val&key=val\n\n"
+           "Examples of URL's:\n"
+           "------------------\n"
+           "TELNET://RC2014.DDNS.NET:2014/\n"
+           "TELNET://BBS.FOZZTEXX.NET:23/\n"
+           "HTTPS://www.gnu.org/licenses/gpl-3.0.txt\n\n");
+
     if (argc == 1) {
         printf("Usage: netcat <URL>\n");
         exit(1);
     }
 
     strcpy(url, argv[1]);
+    printf("URL: %s\n", url);
 
     printf("OPEN URL: %s\n", url);
-    printf("OPEN RETURN: %02X\n", fujinet_network_open(url, 0));
+    printf("OPEN RETURN: %02X\n", fujinet_network_open(src_logical_unit, url, 0));
 
     while(!done) {
         in();
         out();
     }
 
-    fujinet_network_close(url);
+    fujinet_network_close(src_logical_unit);
 
     return 0;
 }
